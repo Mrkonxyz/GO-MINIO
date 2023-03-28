7af1fd8ad7c521e7a72424c2990463a897132637
mrkonxyz <mrkonz@odds.team>
2023-03-28 17:38:45 +0700

    create bugget

--- config/config.go
+++ config/config.go
@@ -7,17 +7,19 @@ var AppConfig Config
 type Config struct {
 	BucketURL         string `mapstructure:"BUCKET_URL"`
 	StorageEndpoint   string `mapstructure:"STORAGE_ENDPOINT"`
-	StorageAccessKey  string `mapstructure:"STORAGE_ACCESS_KEY"`
-	StorageSecretKey  string `mapstructure:"STORAGE_SECRET_KEY"`
+	StorageUser       string `mapstructure:"STORAGE_USER"`
+	StoragePassword   string `mapstructure:"STORAGE_PASSWORD"`
 	StorageBucketName string `mapstructure:"STORAGE_BUCKET_NAME"`
+	StorageSSL        bool   `mapstructure:"USE_SSL"`
 }
 
 func LoadConFig(path string) (config Config) {
 	viper.SetDefault("BUCKET_URL", "http://localhost:9000/")
 	viper.SetDefault("STORAGE_ENDPOINT", "localhost:9000")
-	viper.SetDefault("STORAGE_ACCESS_KEY", "miniouser")
-	viper.SetDefault("STORAGE_SECRET_KEY", "Pa22W0rd")
-	viper.SetDefault("STORAGE_BUCKET_NAME", "news-dev")
+	viper.SetDefault("STORAGE_USER", "miniouser")
+	viper.SetDefault("STORAGE_PASSWORD", "Pa22W0rd")
+	viper.SetDefault("STORAGE_BUCKET_NAME", "images")
+	viper.SetDefault("USE_SSL", false)
 
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/mrkonxyz/config"
+)
+
 func main() {
+
+	cgf := config.LoadConFig(".")
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(cgf.StorageEndpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(cgf.StorageUser, cgf.StoragePassword, ""),
+		Secure: cgf.StorageSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Create a bucket at region 'us-east-1' with object locking enabled.
+	err = minioClient.MakeBucket(context.Background(), cgf.StorageBucketName, minio.MakeBucketOptions{Region: "us-east-1", ObjectLocking: true})
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+
+	fmt.Println("Successfully created mybucket.")
 
 }
